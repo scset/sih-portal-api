90ad306324539c97adf0f55413e5e51ff10e2d46
Manas Khandelwal <mkdpsj@gmail.com>
2023-09-23 21:37:49 +0530

    fix: api key dto body

--- src/core/specialization/specialization.controller.ts
+++ src/core/specialization/specialization.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 
 import { SpecializationService } from "./specialization.service";
 import { ApiKeyDto } from "../../common/dto/api-key.dto";
@@ -8,7 +8,7 @@ export class SpecializationController {
   constructor(private readonly specializationService: SpecializationService) {}
 
   @Post("add")
-  createSpecialization(apiKey: ApiKeyDto) {
-    return this.specializationService.createSpecialization(apiKey);
+  createSpecialization(@Body() apiKeyDto: ApiKeyDto) {
+    return this.specializationService.createSpecialization(apiKeyDto);
   }
 }
